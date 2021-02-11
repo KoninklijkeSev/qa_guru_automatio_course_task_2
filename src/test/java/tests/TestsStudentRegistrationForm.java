828c55bf640c1c0a91efce975041894d4f093b21
KoninklijkeSev <strong.m.m@mail.ru>
2021-02-11 23:28:21 +0300

    Fix comment for homework lesson 2

--- src/test/java/tests/TestsStudentRegistrationForm.java
+++ src/test/java/tests/TestsStudentRegistrationForm.java
@@ -20,33 +20,43 @@ public class TestsStudentRegistrationForm {
     void studentRegistrationForm() {
 
         open("https://demoqa.com/automation-practice-form");
-        $(byText ("Practice Form")).shouldHave(text("Practice Form"));              // проверка, на наличие в main-header текста Practice Form
-        $("#firstName").setValue("First Name");                                      // заополняем имя
-        $("#lastName").setValue("Last Name");                                        // заополняем фамилию
-        $("#userEmail").setValue("useremail@fake.com");                              // заополняем email
+        $(byText ("Practice Form")).shouldHave(text("Practice Form"));
+        $("#firstName").setValue("FirstName");
+        $("#lastName").setValue("LastName");
+        $("#userEmail").setValue("useremail@fake.com");
         // gender-radio-1
-        $(byText("Male")).click();                                                  // отмечаем чекбокс с мужчским полом
-        $("#userNumber").setValue("0123456789");                                     // заополняем номер телефона
-        $("#dateOfBirthInput").click();                                              // заполняем дату рождения
+        $(byText("Male")).click();
+        $("#userNumber").setValue("0123456789");
+        $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("January");
         $(".react-datepicker__year-select").selectOption("2021");
         $(".react-datepicker__day.react-datepicker__day--001").click();
-        $("#subjectsInput").setValue("Maths").pressEnter();                          // заполняем предмет
+        $("#subjectsInput").setValue("Maths").pressEnter();
         // hobbies-checkbox-1
-        $(byText("Sports")).click();                                                // заполняем хобби
+        $(byText("Sports")).click();
         // uploadPicture
-        $("#uploadPicture").uploadFromClasspath("Picture.jpg");             // грузим фото
-//        $("#uploadPicture").pressEnter();
-        $("#currentAddress").setValue("Here Current Address");                       // заполняем адрес
+        $("#uploadPicture").uploadFromClasspath("Picture.jpg");
+        //$("#uploadPicture").pressEnter();
+        $("#currentAddress").setValue("HereCurrentAddress");
         // Select State
-        $("#state").click();                                                         // заполняем штат
+        $("#state").click();
         $(byText("NCR")).click();
         // city
-        $("#city").click();                                                          // заполняем город
+        $("#city").click();
         $(byText("Delhi")).click();
-        $("#submit").click();                                                        // нажимаем на submit
-        // Проверка на отображение формы после заполнения
+        $("#submit").click();
+
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $("tr:nth-of-type(1) > td:nth-of-type(2)").shouldHave(text("FirstName LastName"));
+        $("tr:nth-of-type(2) > td:nth-of-type(2)").shouldHave(text("useremail@fake.com"));
+        $("tr:nth-of-type(3) > td:nth-of-type(2)").shouldHave(text("Male"));
+        $("tr:nth-of-type(4) > td:nth-of-type(2)").shouldHave(text("0123456789"));
+        $("tr:nth-of-type(5) > td:nth-of-type(2)").shouldHave(text("1 January,2021"));
+        $("tr:nth-of-type(6) > td:nth-of-type(2)").shouldHave(text("Maths"));
+        $("tr:nth-of-type(7) > td:nth-of-type(2)").shouldHave(text("Sports"));
+        $("tr:nth-of-type(8) > td:nth-of-type(2)").shouldHave(text("Picture.jpg"));
+        $("tr:nth-of-type(9) > td:nth-of-type(2)").shouldHave(text("HereCurrentAddress"));
+        $("tr:nth-of-type(10) > td:nth-of-type(2)").shouldHave(text("NCR Delhi"));
         $("#closeLargeModal").click();
     }
 
